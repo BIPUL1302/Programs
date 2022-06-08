d0017aec271f9d98d811b0a978917d5a99a2c527
BIPUL1302 <bipulvivek2016@gmail.com>
2022-06-08 10:47:24 +0530

    First Commit

---
+++ program.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void solve();
+
+int main(){
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
+
+void solve(){
+    //code your solution for each test-case here
+    cout<<"Hello";        
+}
+
